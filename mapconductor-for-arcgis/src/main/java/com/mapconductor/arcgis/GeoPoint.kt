a2a96b5c09aaf7114b4bc09b5f8b702d9dd395ec
Masashi Katsumata <wf9a5m75@gmail.com>
2025-05-30 21:59:29 -0700

    Partial fix: ArcGIS / Androidでマーカーが正しくない位置に表示される #4

--- mapconductor-for-arcgis/src/main/java/com/mapconductor/arcgis/GeoPoint.kt
+++ mapconductor-for-arcgis/src/main/java/com/mapconductor/arcgis/GeoPoint.kt
@@ -5,32 +5,38 @@ import com.arcgismaps.geometry.Point
 import com.arcgismaps.geometry.SpatialReference
 import com.mapconductor.core.features.GeoPoint
 
-fun GeoPoint.toPoint(spatialReference: SpatialReference = SpatialReference.wgs84()) : Point = Point(latitude, longitude, altitude, spatialReference)
-fun GeoPoint.Companion.fromLatLongAltitude(latitude: Double, longitude: Double, altitude: Double) = GeoPoint(latitude, longitude, altitude)
-fun GeoPoint.Companion.fromLongLat(longitude: Double, latitude: Double, altitude: Double) = GeoPoint(latitude, longitude, altitude)
+fun GeoPoint.toPoint(spatialReference: SpatialReference = SpatialReference.webMercator()) : Point =
+    Point(x = longitude, y = latitude, z = altitude, spatialReference = spatialReference)
+
+fun GeoPoint.Companion.fromLatLongAltitude(latitude: Double, longitude: Double, altitude: Double) =
+    GeoPoint(latitude = latitude, longitude = longitude, altitude = altitude)
+
+fun GeoPoint.Companion.fromLongLat(longitude: Double, latitude: Double, altitude: Double) =
+    GeoPoint(latitude = latitude, longitude = longitude, altitude = altitude)
+
 fun GeoPoint.Companion.from(point: Point) {
-    val wgs84Point = if (point.spatialReference != SpatialReference.wgs84()) {
-        GeometryEngine.projectOrNull(point, SpatialReference.wgs84()) as Point
+    val wehMercatorPoint = if (point.spatialReference != SpatialReference.webMercator()) {
+        GeometryEngine.projectOrNull(point, SpatialReference.webMercator()) as Point
     } else {
         point
     }
 
     GeoPoint(
-        latitude = wgs84Point.x,
-        longitude = wgs84Point.y,
-        altitude = wgs84Point.z ?: 0.0
+        longitude = wehMercatorPoint.x,
+        latitude = wehMercatorPoint.y,
+        altitude = wehMercatorPoint.z ?: 0.0
     )
 }
 
 fun Point.toGeoPoint(): GeoPoint {
-    val wgs84Point = if (this.spatialReference != SpatialReference.wgs84()) {
-        GeometryEngine.projectOrNull(this, SpatialReference.wgs84()) as Point
+    val wehMercatorPoint = if (this.spatialReference != SpatialReference.webMercator()) {
+        GeometryEngine.projectOrNull(this, SpatialReference.webMercator()) as Point
     } else {
         this
     }
     return GeoPoint(
-        latitude = wgs84Point.x,
-        longitude = wgs84Point.y,
-        altitude = wgs84Point.z ?: 0.0,
+        longitude = wehMercatorPoint.x,
+        latitude = wehMercatorPoint.y,
+        altitude = wehMercatorPoint.z ?: 0.0,
     )
 }
