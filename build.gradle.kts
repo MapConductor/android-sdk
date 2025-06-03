d54c33d7c30072dd4b61f5ff83e130ea500db1ec
Masashi Katsumata <wf9a5m75@gmail.com>
2025-06-03 22:16:17 +0900

    Introduce KtLint

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
+    id("org.jlleitschuh.gradle.ktlint") version "13.0.0-rc.1"
 }
 
 buildscript {
@@ -13,5 +14,14 @@ buildscript {
     }
     dependencies {
         classpath(libs.secrets.gradle.plugin)
+    }
+}
+ktlint {
+    debug.set(false)
+    android.set(true)
+    outputColorName.set("RED")
+    reporters {
+        reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.PLAIN)
+        reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.CHECKSTYLE)
     }
 }
