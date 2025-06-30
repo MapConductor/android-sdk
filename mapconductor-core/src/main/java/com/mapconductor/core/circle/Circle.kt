c26ef32991bf8dcb9530fb10a747a76824423d98
KouTsukigase <kou.tsukigase@gmail.com>
2025-06-30 15:41:57 -0700

    CircleにfingerPrintを実装

--- mapconductor-core/src/main/java/com/mapconductor/core/circle/Circle.kt
+++ mapconductor-core/src/main/java/com/mapconductor/core/circle/Circle.kt
@@ -3,20 +3,71 @@ package com.mapconductor.core.circle
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.room.util.copy
+import androidx.compose.runtime.snapshotFlow
 import com.mapconductor.core.features.IGeoPoint
 import android.graphics.Color
+import android.os.Parcelable
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
 
 class CircleState(
     center: IGeoPoint,
     radius: Int,
     strokeColor: Int = Color.RED,
-    strokeWidth: Float = 2.0f,
+    strokeWidth: Int = 2,
     fillColor: Int = Color.argb(127, 255, 255, 255),
+    id: String? = null,
+    extra: Parcelable? = null,
 ) {
     var center by mutableStateOf(center)
     var radius by mutableStateOf(radius)
     var strokeColor by mutableStateOf(strokeColor)
     var strokeWidth by mutableStateOf(strokeWidth)
     var fillColor by mutableStateOf(fillColor)
+    var extra by mutableStateOf(extra)
+
+    val id =
+        (
+                id ?: circleId(
+                    listOf(
+                        center.hashCode(),
+                        radius,
+                        extra?.hashCode() ?: 0,
+                        strokeColor,
+                        strokeWidth,
+                        fillColor,
+                    ),
+                )
+                ).toString()
+
+    private fun circleId(hashCodes: List<Int>): Int =
+        hashCodes.reduce { result, hashCode ->
+            31 * result + hashCode
+        }
+
+    fun fingerPrint(): CircleFingerPrint {
+        return CircleFingerPrint(
+            id = this.id.hashCode(),
+            center = center.hashCode(),
+            radius = radius,
+            strokeColor = strokeColor,
+            strokeWidth = strokeWidth,
+            fillColor = fillColor,
+            extra = extra?.hashCode() ?: 0,
+        )
+    }
+
+    fun asFlow(): Flow<CircleFingerPrint> = snapshotFlow { fingerPrint() }.distinctUntilChanged()
 }
+
+data class CircleFingerPrint(
+    val id: Int,
+    val center: Int,
+    val radius: Int,
+    val strokeColor: Int,
+    val strokeWidth: Int,
+    val fillColor: Int,
+    val extra: Int,
+)
+
+typealias OnCircleEventHandler = (CircleState) -> Unit
