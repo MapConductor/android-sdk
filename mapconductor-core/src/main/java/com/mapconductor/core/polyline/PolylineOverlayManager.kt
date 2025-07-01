90ae40c148d421c6c9056e869fa565b091419295
Masashi Katsumata <wf9a5m75@gmail.com>
2025-07-01 16:02:48 -0700

    Implementation of Polyline Drawing Function (Continued)

--- mapconductor-core/src/main/java/com/mapconductor/core/polyline/PolylineOverlayManager.kt
+++ mapconductor-core/src/main/java/com/mapconductor/core/polyline/PolylineOverlayManager.kt
@@ -1,4 +1,24 @@
 package com.mapconductor.core.polyline
 
-class PolylineOverlayManager {
+interface PolylineOverlayManager {
+    suspend fun addPolylines(polylines: List<PolylineState>)
+
+    suspend fun updatePolyline(polyline: PolylineState)
+
+    fun getPolylineState(id: String): PolylineState?
+}
+
+class PolylineOverlayManagerImpl<ActualPolyline>: PolylineOverlayManager {
+    val polylineEntities = mutableListOf<PolylineEntity<ActualPolyline>>()
+    override suspend fun addPolylines(polylines: List<PolylineState>) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updatePolyline(polyline: PolylineState) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getPolylineState(id: String): PolylineState? {
+        TODO("Not yet implemented")
+    }
 }
