75243625b43cf265a1820c4643e40f2049fad931
f-sara <jyujyusa.68@gmail.com>
2025-09-25 00:32:03 +0900

    feat: RightTailInfoBubbleIconのビットマップを作成

--- mapconductor-icons/src/main/java/com/mapconductor/icons/RightTailInfoBubbleIcon.kt
+++ mapconductor-icons/src/main/java/com/mapconductor/icons/RightTailInfoBubbleIcon.kt
@@ -4,16 +4,25 @@ import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.core.content.ContextCompat
+import com.mapconductor.core.BitmapIconCache
 import com.mapconductor.core.R
+import com.mapconductor.core.ResourceProvider
 import com.mapconductor.core.marker.AbstractMarkerIcon
 import com.mapconductor.core.marker.BitmapIcon
 import com.mapconductor.settings.MarkerIconSize
+import android.graphics.Bitmap
+import android.graphics.Bitmap.createBitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
 import android.graphics.drawable.Drawable
 
 class RightTailInfoBubbleIcon(
@@ -85,7 +94,81 @@ class RightTailInfoBubbleIcon(
     override fun toString(): String = "RightTailInfoBubbleIcon($properties)"
 
     override fun toBitmapIcon(): BitmapIcon {
-        return TODO("ビットマップを返却")
+        val id = "right_tail_info_bubble_icon${hashCode()}".hashCode()
+        BitmapIconCache.get(id)?.let {
+            return it
+        }
+
+        val drawableSize = ResourceProvider.dpToPx(iconSize.value * scale).toFloat()
+        val drawableInnerPadding = drawableSize * 0.1f
+        val contentMargin = drawableSize * 0.2f
+
+        val labelPaint = Paint().apply {
+            isAntiAlias = true
+            color = labelTextColor.toArgb()
+            textSize = drawableSize * 0.7f
+        }
+        val labelWidth = labelPaint.measureText(label)
+        val labelHeight = labelPaint.fontMetrics.run { bottom - top }
+
+        val snippetPaint = Paint().apply {
+            isAntiAlias = true
+            color = Color.LightGray.toArgb()
+            textSize = drawableSize * 0.4f
+        }
+        val snippetHeight = snippetPaint.fontMetrics.run { bottom - top }
+
+        val canvasWidth = drawableSize + drawableInnerPadding + labelWidth + contentMargin * 2
+        val canvasHeight = maxOf(drawableSize, labelHeight) + drawableInnerPadding + snippetHeight + drawableInnerPadding * 2
+        val pointerWidth = canvasWidth / 9f
+        val pointerHeight = canvasHeight / 8f
+
+        val bitmap = createBitmap(
+            canvasWidth.toInt(),
+            (canvasHeight + pointerHeight).toInt(),
+            Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(bitmap)
+
+        val backgroundPaint = Paint().apply {
+            color = fillColor.toArgb()
+            style = Paint.Style.FILL
+            isAntiAlias = true
+        }
+
+        val backgroundPath = Path().apply {
+            addRect(
+                0f, 0f, canvasWidth, canvasHeight,
+                Path.Direction.CW
+            )
+
+            // 吹き出しの矢印
+            moveTo(canvasWidth - pointerWidth * 2f, canvasHeight)
+            lineTo(canvasWidth - pointerWidth, canvasHeight)
+            lineTo(canvasWidth - pointerWidth * 1.5f, canvasHeight + pointerHeight)
+            close()
+        }
+
+        canvas.drawPath(backgroundPath, backgroundPaint)
+
+        iconDrawable.setBounds(
+            contentMargin.toInt(), contentMargin.toInt(), (drawableInnerPadding + drawableSize).toInt(), (drawableInnerPadding + drawableSize).toInt()
+        )
+        iconDrawable.draw(canvas)
+
+        val labelX = contentMargin + drawableSize + drawableInnerPadding
+        val labelY = contentMargin + drawableSize / 2f + labelHeight / 2f - labelPaint.fontMetrics.bottom
+        canvas.drawText(label, labelX, labelY, labelPaint)
+
+        canvas.drawText(snippet, contentMargin, canvasHeight - contentMargin, snippetPaint)
+
+        val result = BitmapIcon(
+            bitmap = bitmap, anchor = Offset(0.5f, 1.0f),
+            size = Size(canvasWidth, canvasHeight + pointerHeight)
+        )
+
+        BitmapIconCache.put(id, result)
+        return result
     }
 }
 
@@ -99,7 +182,7 @@ fun RightTailInfoBubbleIconPreview() {
             iconDrawable = ContextCompat.getDrawable(context, R.drawable.default_marker)!!,
             label = "5時間37分",
             snippet = "304マイル",
-            fillColor = Color.LightGray,
+            fillColor = Color.White,
             labelTextColor = Color.Yellow,
             scale = 1f,
             iconSize = MarkerIconSize.Small,
