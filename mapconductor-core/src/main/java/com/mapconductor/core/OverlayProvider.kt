b81ad7b6d6c470ee4f660ce5403d9164429519c9
Masashi Katsumata <wf9a5m75@gmail.com>
2025-10-15 00:11:39 +0900

    Remove unused coroutine

--- mapconductor-core/src/main/java/com/mapconductor/core/OverlayProvider.kt
+++ mapconductor-core/src/main/java/com/mapconductor/core/OverlayProvider.kt
@@ -26,7 +26,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
 open class MapViewScope {
-    val overflowScope = CoroutineScope(Dispatchers.IO)
     val markerAddSharedFlow = MutableSharedFlow<MarkerState>(1000)
     val markerFlow = MutableStateFlow<MutableMap<String, MarkerState>>(mutableMapOf())
     val bubbleFlow = MutableStateFlow<MutableMap<String, InfoBubbleEntry>>(mutableMapOf())
@@ -67,42 +66,6 @@ fun CollectAndRenderOverlays(
     registry.getAll().forEach { overlay ->
         @Suppress("UNCHECKED_CAST")
         val typedOverlay = overlay as MapOverlay<Any>
-
-//        LaunchedEffect(Unit) {
-//            typedOverlay.flow
-//                .debounce(100) // Debounce updates for 100ms to prevent excessive rendering
-//                .collect { items ->
-// //                    typedOverlay.render(items, controller)
-//
-//                    if (items.isNotEmpty()) {
-//                        return@collect
-//                    }
-//
-//                    // Process items in chunks to prevent main thread blocking
-//                    val chunks = items.values.chunked(500) // Process 50 items at a time
-//
-//                    chunks.forEach { chunk ->
-//                        val chunkMap =
-//                            chunk
-//                                .associateBy {
-//                                    when (it) {
-//                                        is MarkerState -> it.id
-//                                        is CircleState -> it.id
-//                                        is PolylineState -> it.id
-//                                        is PolygonState -> it.id
-//                                        is GroundImageState -> it.id
-//                                        else -> it.toString()
-//                                    }
-//                                }.toMutableMap()
-//
-//                        typedOverlay.render(chunkMap, controller)
-//
-//                        // Yield to allow other coroutines and UI updates
-//                        yield()
-//                    }
-//                }
-//        }
-
         val flowState = typedOverlay.flow.collectAsState()
 
         LaunchedEffect(flowState.value) {
