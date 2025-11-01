a76acf22cf1557810ad30ef523ef4f8235110726
Masashi Katsumata <wf9a5m75@gmail.com>
2025-11-02 03:30:22 +0900

    fix: Marker remains in the dragging mode after marker is dropped

--- mapconductor-for-mapbox/src/main/java/com/mapconductor/mapbox/marker/MapboxMarkerController.kt
+++ mapconductor-for-mapbox/src/main/java/com/mapconductor/mapbox/marker/MapboxMarkerController.kt
@@ -31,6 +31,7 @@ class MapboxMarkerController(
                     markerManager.registerEntity(it)
                     renderer.redraw()
                 }
+                internalSelectedMarker = null
                 return
             }
             internalSelectedMarker = value
