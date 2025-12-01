a2a4afa1d6aa5cbd05ee64a83a9b707ca2d8bf4c
Masashi Katsumata <wf9a5m75@gmail.com>
2025-12-01 18:00:24 +0900

    maven publish test

--- mapconductor-bom/build.gradle.kts
+++ mapconductor-bom/build.gradle.kts
@@ -121,8 +121,8 @@ publishing {
     }
 }
 
-if (project.hasProperty("signing.keyId")) {
-    signing {
-        sign(publishing.publications["release"])
-    }
+signing {
+    // Require signing properties to be set
+    isRequired = true
+    sign(publishing.publications["release"])
 }
