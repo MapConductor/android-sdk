46eec0d31f0038bfb8975f0ee0c18d848192f109
masashi katsumata <wf9a5m75@gmail.com>
2025-12-29 12:27:47 +0900

    fix: Align the class name with others: "MapLibreDesignType"

--- mapconductor-for-maplibre/src/main/java/com/mapconductor/maplibre/MapLibreDesign.kt
+++ mapconductor-for-maplibre/src/main/java/com/mapconductor/maplibre/MapLibreDesign.kt
@@ -6,7 +6,7 @@ interface MapLibreMapDesignType : MapDesignType<String> {
     val styleJsonURL: String
 }
 
-data class MapLibreDesignType(
+data class MapLibreDesign(
     override val id: String,
     override val styleJsonURL: String,
 ) : MapLibreMapDesignType {
@@ -14,7 +14,7 @@ data class MapLibreDesignType(
 
     companion object {
         val DemoTiles =
-            MapLibreDesignType(
+            MapLibreDesign(
                 id = "demo",
                 styleJsonURL = "https://demotiles.maplibre.org/style.json",
             )
