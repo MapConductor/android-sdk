7ea461368f518afca7bef4a0584080bee33c8bf6
Masashi Katsumata <wf9a5m75@gmail.com>
2026-06-15 01:44:44 +0900

    feat: add android-geojson-layer submodule and integrate GeoJSON layer example into simple-map-app

--- simple-map-app/src/main/java/com/mapconductor/simplemapapp/MainActivity.kt
+++ simple-map-app/src/main/java/com/mapconductor/simplemapapp/MainActivity.kt
@@ -39,6 +39,7 @@ import com.mapconductor.core.marker.DefaultMarkerIcon
 import com.mapconductor.core.marker.Marker
 import com.mapconductor.core.marker.MarkerAnimation
 import com.mapconductor.core.marker.MarkerState
+import com.mapconductor.core.marker.MarkerTilingOptions
 import com.mapconductor.core.marker.Markers
 import com.mapconductor.core.polygon.Polygon
 import com.mapconductor.core.polygon.PolygonState
@@ -56,29 +57,34 @@ import com.mapconductor.mapbox.rememberMapboxMapViewState
 import com.mapconductor.maplibre.MapLibreDesign
 import com.mapconductor.maplibre.MapLibreMapView
 import com.mapconductor.maplibre.rememberMapLibreMapViewState
+import com.mapconductor.geojson.GeoJSONFeature
+import com.mapconductor.geojson.GeoJSONLayer
+import com.mapconductor.geojson.GeoJSONLayerState
+import com.mapconductor.geojson.GeoJSONParser
 import com.mapconductor.simplemapapp.postoffice.HeatmapLayerPageViewModel
 import com.mapconductor.simplemapapp.postoffice.HeatmapLayerViewModelInterface
 import com.mapconductor.simplemapapp.postoffice.PostOfficeDataLoader
 import com.mapconductor.simplemapapp.ui.theme.MapConductorSDKTheme
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.util.zip.ZipInputStream
+import android.util.Log
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val image = ContextCompat.getDrawable(this, R.drawable.overlayimg)!!
+        // val image = ContextCompat.getDrawable(this, R.drawable.overlayimg)!!
+
+
 
         setContent {
             MapConductorSDKTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TokyoExample(
-                        modifier =
-                            Modifier
-                                .padding(innerPadding)
-                                .fillMaxSize(),
-                    )
+                    SimpleMap(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -111,404 +117,86 @@ val goryokaku = listOf(
 )
 
 @Composable
-fun TokyoExample(modifier: Modifier = Modifier) {
-    var selectedMarker by remember { mutableStateOf<MarkerState?>(null) }
+fun SimpleMap(modifier: Modifier) {
 
-    val center = GeoPoint(
-        latitude = 35.6762,
-        longitude = 139.6503,
-    )
-
-    val mapViewState =
-        rememberHereMapViewState(
-            cameraPosition =
-                MapCameraPosition(
-                    position = center,
-                    zoom = 2.0,
-                ),
-        )
-
-    val markerState = remember { MarkerState(
-        position = center,
-        icon = DefaultMarkerIcon().copy(
-            label = "Tokyo",
+    val mapViewState = rememberMapLibreMapViewState(
+        cameraPosition = MapCameraPosition(
+            position = GeoPoint(35.68, 139.77),
+            zoom = 13.0,
         ),
-        onClick = {
-            selectedMarker = it
-        }
-    ) }
-
-    HereMapView(
-        state = mapViewState,
-        modifier = modifier,
-    ) {
-        Marker(markerState)
-
-        selectedMarker?.let {
-            InfoBubble(
-                marker = it,
-            ) {
-                Text("Hello, world!")
-            }
-        }
-    }
-}
-
-@Composable
-fun MapviewExample(modifier: Modifier = Modifier) {
-    val initCameraPosition = MapCameraPosition(
-        position = GeoPoint(37.422198, -122.085377),
-        zoom = 18.0,
-        tilt = 60.0,
-        bearing = 30.0,
     )
-    val mapViewState = rememberGoogleMapViewState(
-        cameraPosition = initCameraPosition,
-    )
-
-    GoogleMapView(
-        state = mapViewState,
-        modifier = modifier,
-    ) {
-    }
-}
-
-@Composable
-fun MarkerExample(modifier: Modifier = Modifier) {
-    var selectedMarker by remember { mutableStateOf<MarkerState?>(null) }
-
-    val initCameraPosition = MapCameraPosition(
-        position = GeoPoint(37.422198, -122.085377),
-        zoom = 18.0,
-    )
-    val mapViewState = rememberGoogleMapViewState(
-        cameraPosition = initCameraPosition,
-    )
-    val markerState = remember { MarkerState(
-        position = GeoPoint(37.422198, -122.085377),
-        icon = DefaultMarkerIcon().copy(
-            label = "GoogleMaps"
-        ),
-        onClick = {
-            selectedMarker = it
-        },
-        ) }
-
-    GoogleMapView(
-        state = mapViewState,
-        modifier = modifier,
-    ) {
-//        selectedMarker?.let {
-//            InfoBubble(
-//                marker = it,
-//            ) {
-//                Text("Hello, world!")
-//            }
-//        }
-
-        Marker(markerState)
-    }
-}
-
-@Composable
-fun PolylineExample(modifier: Modifier = Modifier) {
-    val initCameraPosition = MapCameraPosition(
-        position = GeoPoint(41.79,140.75),
-        zoom = 3.0,
-    )
-    val mapViewState = rememberGoogleMapViewState(
-        cameraPosition = initCameraPosition,
-    )
-
-    val polylineState = remember { PolylineState(
-        points = airpots,
-        strokeColor = Color.Blue.copy(alpha = 0.5f),
-        strokeWidth = 4.dp,
-        geodesic = true,
-    ) }
-
-    GoogleMapView(mapViewState) {
-        Polyline(polylineState)
-    }
-}
-@Composable
-fun CirleExample(modifier: Modifier = Modifier) {
-    val initCameraPosition = MapCameraPosition(
-        position = GeoPoint(37.422198, -122.085377),
-        zoom = 17.0,
-    )
-    val mapViewState = rememberGoogleMapViewState(
-        cameraPosition = initCameraPosition,
-    )
-    val circleState = remember { CircleState(
-        center = GeoPoint(37.422198, -122.085377),
-        radiusMeters = 50.0,
-        fillColor = Color.Blue.copy(alpha = 0.5f),
-    )
-    }
-
-    GoogleMapView(
-        state = mapViewState,
-        modifier = modifier,
-    ) {
-        Circle(circleState)
-    }
-}
-
-@Composable
-fun PolygonExample(modifier: Modifier = Modifier) {
-    val initCameraPosition = MapCameraPosition(
-        position = GeoPoint(41.79,140.75),
-        zoom = 14.0,
-//        tilt = 60.0,
-//        bearing = 30.0,
-    )
-    val mapViewState = rememberHereMapViewState(
-        cameraPosition = initCameraPosition,
-    )
-
-    val polygonState = remember { PolygonState(
-        points = goryokaku,
-        strokeColor = Color.Blue.copy(alpha = 0.5f),
-        fillColor =  Color.Red.copy(alpha = 0.7f),
-    ) }
-
-    HereMapView(mapViewState) {
-        Polygon(polygonState)
-    }
-}
-
-@Composable
-fun GroundImageExample(modifier: Modifier = Modifier, image: Drawable) {
-    val initCameraPosition = MapCameraPosition(
-        position = GeoPoint(51.511649,-0.100761),
-        zoom = 11.0,
-    )
-    val mapViewState = rememberArcGISMapViewState(
-        cameraPosition = initCameraPosition,
-    )
-
-    val groundImageState = remember { GroundImageState(
-        bounds = GeoRectBounds(
-            southWest = GeoPoint.fromLatLong(51.476747, -0.167729),
-            northEast = GeoPoint.fromLatLong(51.546550,-0.033792),
-        ),
-        image = image,
-        opacity = 0.5f,
-    )
-    }
-
-    ArcGISMapView(
-        state = mapViewState,
-        modifier = modifier,
-    ) {
-        GroundImage(groundImageState)
-    }
-}
-
-@Composable
-fun HolePolygonExample(modifier: Modifier = Modifier) {
-    val mapViewState =
-        rememberMapLibreMapViewState(
-            cameraPosition =
-                MapCameraPosition(
-                    position = GeoPoint(43.06050568387817, 141.35374551567804),
-                    zoom = 11.0,
-                ),
-        )
-
-    val polygonState =
-        remember {
-            PolygonState(
-                points =
-                    listOf(
-                        GeoPoint(85.0, 90.0),
-                        GeoPoint(85.0, 0.1),
-                        GeoPoint(85.0, -90.0),
-                        GeoPoint(85.0, -179.9),
-                        GeoPoint(0.0, -179.9),
-                        GeoPoint(-85.0, -179.9),
-                        GeoPoint(-85.0, -90.0),
-                        GeoPoint(-85.0, 0.1),
-                        GeoPoint(-85.0, 90.0),
-                        GeoPoint(-85.0, 179.9),
-                        GeoPoint(0.0, 179.9),
-                        GeoPoint(85.0, 179.9),
-                    ),
-                holes =
-                    listOf(
-                        listOf( // 1
-                            GeoPoint(43.10086924222251, 141.35290903949243),
-                            GeoPoint(43.04444342582366, 141.4118953480885),
-                            GeoPoint(43.05060149394299, 141.30656265416695),
-                        ),
-                        listOf( // 2
-                            GeoPoint(43.06035050410283, 141.31990479539704),
-                            GeoPoint(43.038284739487004, 141.33324693662706),
-                            GeoPoint(43.049062034871525, 141.28690055130158),
-                        ),
-                    ),
-                fillColor = Color(0xCC787880),
-                strokeColor = Color.Red,
-                strokeWidth = 2.dp,
-            )
-        }
-
-//    val polygonState2 = remember { tokyoPolygonState }
 
     MapLibreMapView(
         state = mapViewState,
         modifier = modifier,
+        onMapLoaded = {
+            Log.d("SimpleMap", "---->loaded")
+        }
     ) {
-        Polygon(polygonState)
-//        Polygon(polygonState2)
+        Marker(
+            position = GeoPoint(35.68, 139.77),
+            onClick = {
+                Log.d("SimpleMap", "--->clicked")
+            }
+        )
     }
 }
 
-// @Composable
-// fun BasicGeoJSONExample(
-//    modifier: Modifier = Modifier,
-// ) {
-//    val context = LocalContext.current
-//    val mapViewState =
-//        rememberMapLibreMapViewState(
-//            cameraPosition =
-//                MapCameraPosition(
-//                    position = GeoPoint(35.691153, 139.756878),
-//                    zoom = 10.0,
-//                )
-//        )
-// //    val geoJson =
-// //        remember {
-// //            // A small sample with MultiPoint, MultiLineString and MultiPolygon (lon/lat order).
-// //            """
-// //            {
-// //              "type": "FeatureCollection",
-// //              "features": [
-// //                {
-// //                  "type": "Feature",
-// //                  "id": "multipoint",
-// //                  "properties": { "name": "MultiPoint sample" },
-// //                  "geometry": {
-// //                    "type": "MultiPoint",
-// //                    "coordinates": [
-// //                      [139.76669, 35.68049],
-// //                      [139.75688, 35.69115],
-// //                      [139.78100, 35.67350]
-// //                    ]
-// //                  }
-// //                },
-// //                {
-// //                  "type": "Feature",
-// //                  "id": "multiline",
-// //                  "properties": { "name": "MultiLineString sample" },
-// //                  "geometry": {
-// //                    "type": "MultiLineString",
-// //                    "coordinates": [
-// //                      [[139.74, 35.69], [139.77, 35.69], [139.80, 35.69]],
-// //                      [[139.76, 35.66], [139.76, 35.68], [139.76, 35.70]]
-// //                    ]
-// //                  }
-// //                },
-// //                {
-// //                  "type": "Feature",
-// //                  "id": "multipolygon",
-// //                  "properties": { "name": "MultiPolygon sample" },
-// //                  "geometry": {
-// //                    "type": "MultiPolygon",
-// //                    "coordinates": [
-// //                      [
-// //                        [
-// //                          [139.7500, 35.6750],
-// //                          [139.7800, 35.6750],
-// //                          [139.7800, 35.6950],
-// //                          [139.7500, 35.6950],
-// //                          [139.7500, 35.6750]
-// //                        ]
-// //                      ]
-// //                    ]
-// //                  }
-// //                }
-// //              ]
-// //            }
-// //            """.trimIndent()
-// //        }
-//    val geoJsonInputStream = remember {
-//        context.assets.open("prefectures.geojson")
-//    }
-//
-//    MapLibreMapView(
-//        modifier = modifier,
-//        state = mapViewState,
-//        markerTiling = MarkerTilingOptions.Default.copy(
-//            minMarkerCount = 0,
-//            debugTileOverlay = true,
-//        ),
-//    ) {
-//        GeoJsonLayer(
-//            geoJsonInputStream = geoJsonInputStream,
-//            geoJsonKey = geoJsonInputStream.hashCode(),
-//            style =
-//                GeoJsonLayerStyle(
-//                    polylineStrokeColor = Color(0xFF1565C0),
-//                    polylineStrokeWidth = 3.dp,
-//                    polygonStrokeColor = Color(0xFF2E7D32),
-//                    polygonStrokeWidth = 2.dp,
-//                    polygonFillColor = Color(0x332E7D32),
-//                ),
-//        )
-//    }
-// }
-
 @Composable
-fun HeatmapExample(modifier: Modifier = Modifier) {
+fun GeoJsonLayerExample(modifier: Modifier = Modifier) {
     val context = LocalContext.current
-    val dataLoader = remember { PostOfficeDataLoader(context) }
 
-    val viewModel: HeatmapLayerViewModelInterface =
-        viewModel<HeatmapLayerPageViewModel>(
-            factory =
-                object : ViewModelProvider.Factory {
-                    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-                        if (modelClass.isAssignableFrom(HeatmapLayerPageViewModel::class.java)) {
-                            @Suppress("UNCHECKED_CAST")
-                            return HeatmapLayerPageViewModel(
-                                dataLoader = dataLoader,
-                            ) as T
-                        }
-                        throw IllegalArgumentException("Unknown ViewModel class")
-                    }
-                },
+    val mapViewState = rememberMapLibreMapViewState(
+        cameraPosition = MapCameraPosition(
+            position = GeoPoint(35.68, 139.77),
+            zoom = 13.0,
+        ),
+    )
+
+    var selectedFeature by remember { mutableStateOf<GeoJSONFeature?>(null) }
+    var tappedPosition by remember { mutableStateOf<GeoPoint?>(null) }
+
+    val layerState = remember {
+        GeoJSONLayerState(
+            strokeColor = android.graphics.Color.argb(220, 30, 136, 229),
+            fillColor = android.graphics.Color.argb(60, 30, 136, 229),
+            strokeWidth = 1.5f,
+            onClick = { feature, position ->
+                selectedFeature = feature
+                tappedPosition = GeoPoint.from(position)
+            },
         )
-    LaunchedEffect(Unit) {
-        viewModel.loadPostOfficeData()
     }
-    val points = viewModel.heatmapPoints.collectAsState().value
 
-    val camera =
-        remember {
-            MapCameraPosition(
-                position =
-                    GeoPoint.fromLatLong(
-                        latitude = 35.68049,
-                        longitude = 139.76669,
-                    ),
-                zoom = 10.0,
-                bearing = 0.0,
-                tilt = 0.0,
-                paddings = null,
-            )
+    var features by remember { mutableStateOf<List<GeoJSONFeature>>(emptyList()) }
+
+    LaunchedEffect(Unit) {
+        features = withContext(Dispatchers.IO) {
+            ZipInputStream(context.assets.open("tokyo-6-areas.zip")).use { zis ->
+                zis.nextEntry ?: return@withContext emptyList()
+                GeoJSONParser.parseStream(zis)
+            }
         }
-    val mapViewState = rememberHereMapViewState(cameraPosition = camera)
+    }
 
-//    HereMapView(
-//        state = mapViewState,
-//        modifier = modifier,
-//    ) {
-//        HeatmapOverlay(
-//            trackPointUpdates = false,
-//        ) {
-//            HeatmapPoints(points)
-//        }
-//    }
+    MapLibreMapView(
+        state = mapViewState,
+        modifier = modifier,
+        onMapClick = {
+            if (!layerState.processClick(it)) {
+                tappedPosition = null
+                selectedFeature = null
+            }
+        },
+    ) {
+        GeoJSONLayer(state = layerState, features = features)
+
+        tappedPosition?.let { pt ->
+            InfoBubble(position = pt) {
+                selectedFeature?.properties?.let {
+                    TableView(it, 0.3f, 0.7f)
+                }
+            }
+        }
+    }
 }
